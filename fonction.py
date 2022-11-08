50159962f4cd14426daf47990412caa39e4c83c7
Xevione <bturcat@gaming.tech>
2022-11-08 11:03:14 +0100

    cours

--- fonction.py
+++ fonction.py
@@ -20,6 +20,7 @@ def divide(x,y):
     if y ==0:
     #Alors on affiche que ce n'est pas possible
         print('pas possible')
+        return
     #Sinon, pas de probleme
     else:
     #on divise le premier element par le deuxieme
@@ -61,3 +62,73 @@ def calculeSalaireNet(salairBrut, public):
     #SalairNet = SalairBrut * 0.77
     #Je renvoie le salair net
     #return SalairNet
+
+tour = 0
+# Tant que je ne suis pas au tour 5
+while tour != 5 :
+# Appeler la fonction JouerUnTour
+# J'effectue l'action de passer un tour
+    tour = tour + 1
+
+
+
+#Exercice:
+    #Faire un mini jeu qui affiche un message lorsque input renvoie le bon caractere
+    # le caractere doit etre parametrable
+def devine(x):
+    #nombre de tentative
+    tenta = 0
+    # Je definie une variable char qui permet de contenir le caracère genere avec input
+    char = None
+    #Tant que l'input de la lettre n'est pas egal au caractere string au hasard ou tentative > 10 alors
+    while (char!= x) :
+         #Je redéfinis la variable char par un caractere genere au hasard avec input
+        char = input()
+        #J'incremente la variable tenta
+        tenta = tenta + 1
+    # J'affiche le nombre de tentatives
+    print(tenta)
+
+       
+
+def miniGame(winCondition):
+    # Je definie une variable char qui permet de contenir le caracère genere avec input
+    char = None
+    # Je definie une variable tries pour savoir le nombre d'essaies réalisé
+    #genere le caractère winCondition
+    tries = 0
+    #Tant que la variable char n'est pas égal au caractère winCondition
+    while (char != winCondition):
+        #Je redéfinis la variable char par un caractere genere au hasard avec input
+        char = input()
+        #J'incremente la variable tries
+        tries = tries + 1
+    #J'affiche la variable tries
+    print(tries)
+
+tablo =[0,15,3,0,5,7,9]
+#Pour récupérer le 3
+tablo[2]
+#Pour la longeur du tableau
+len(tablo)
+
+
+prenom = "Benjamin"
+nom = "Turcat"
+
+nomPrenom = nom + prenom #Renvoie TurcatBenjamin
+nomPrenom = nom + " " + prenom # Revoie Turcat Benjamin
+intergerValue = 342
+stringIntergerValue = str(342) #Renvoie "342" au lieu de 342
+#Exo
+#Faire une fonction qui concatene 2 chaines de caractere, les séparants par une virgule
+
+#EXO 2:
+#Faire une fonction qui itere sur tous les index d'un tableau renvoyant un chaine de caractere
+#avec l'enssemble des occuration d'un chiffre e.g :
+#pour le tableau =[0,1,1,1,0,1,1,0,1]
+#la fonction (tableau,0) doit renvoyer "0,4,7" n'hesitez pas a implementer la premiere fonction
+def indexTableau(tab):
+    i = 0
+    while i < len(tab):
+        print (tab[i])
